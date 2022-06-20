10bb61e7051ef27371bb52750338a05ac4a7df38
Lewis Kirori <88181151+lewiskirori@users.noreply.github.com>
2022-06-20 16:38:36 +0300

    Add files via upload
    add featured products & product description

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const root = document.getElementById("root");
 
-const menu = ["Home", "About", "Contact", "Products", "Login"];
-
+const menu = ["Home", "About", "Contact", "Products", "Cart", "Login"];
+const Products = [
+  {
+    id: 1,
+    name: "Product 1",
+    price: 150,
+    description: "Description 1",
+    featured: true,
+    image: "https://via.placeholder.com/150",
+  },
+];
 function nav(menu) {
   const navBar = document.createElement("nav");
   for (let item in menu) {
@@ -13,5 +22,55 @@ function nav(menu) {
   return navBar;
 }
 
-root.appendChild(nav(menu));
+function hero() {
+  let hero = document.createElement("div");
+  hero.className = "hero";
+  let h1 = document.createElement("h1");
+  h1.innerHTML = "Welcome to the store";
+  hero.appendChild(h1);
+  return hero;
+}
+
+function featuredProducts(products) {
+  let featured = document.createElement("div");
+  featured.className = "featured";
+  let h2 = document.createElement("h2");
+  h2.innerHTML = "featured Products";
+  featured.appendChild(h2);
+  const productsContainer = document.createElement("div");
+  productsContainer.className = "grid";
+  products.forEach(function (product) {
+    const productContainer = document.createElement("div");
+    productContainer.className = "product";
+    const productImage = document.createElement("img");
+    productImage.src = product.image;
+    productContainer. appendChild(productImage);
+    const productName = document.createElement("h3");
+    productName.innerHTML = product.name; 
+    productContainer.appendChild(productName);
+    const productPrice = document.createElement("p");
+    productPrice.innerHTML = product.price;
+    productContainer.appendChild(productPrice);
+    const productDescription = document.createElement("p");
+    productDescription.innerHTML = product.description;
+    productContainer.appendChild(productDescription);
+    productsContainer.appendChild(productContainer);
+  });
   
+  featured.appendChild(productsContainer);
+  return featured;
+}
+
+function footer() {
+  let footer = document.createElement("footer");
+  footer.className = "footer";
+  let p = document.createElement("p");
+  p.innerHTML = "Shopping &copy; 2022";
+  footer.appendChild(p);
+  return footer;
+}
+
+root.appendChild(nav(menu));
+root.appendChild(hero());
+root.appendChild(featuredProducts());
+root.appendChild(footer());
